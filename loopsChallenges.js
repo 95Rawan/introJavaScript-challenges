6dcbcc7ffb3ef81b0699719371e1537bdac9b75f
maysqunaibi <maysqunaibi2000@gmail.com>
2020-03-18 16:30:54 +0200

    loops

---
+++ loopsChallenges.js
@@ -0,0 +1,19 @@
+/**
+ * what is the output of this code?
+    var x = 0;
+    while(x < 6){
+        x++
+    }
+    console.log(X)
+    //output:____
+ * Print EVEN values from 0 to 10 using for loop.
+ * print ODD values from 0 to 10 using while loop.
+ * what is the output of this code?
+ * var y = 8;
+   do{
+        x++
+    }
+    while(x > 10)
+    console.log(X)
+    //output:____
+ */
