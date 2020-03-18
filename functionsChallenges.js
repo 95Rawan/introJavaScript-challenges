33433d693b4df29f616febcfddc761a70a5f183a
maysqunaibi <maysqunaibi2000@gmail.com>
2020-03-18 17:50:57 +0200

    functions challenges

---
+++ functionsChallenges.js
@@ -0,0 +1,8 @@
+/**
+ * write a function that takes an array as a prameter and doubled the elements of this array:
+ *  [1,2,3,4] => output: [1,4,6,8]
+
+ * Write a JavaScript function that returns array elements larger than a number.
+ * largerThan([44,15,28,55,32], 30) => [32,44,55]
+ 
+ */
