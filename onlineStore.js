03c043a9e9096f11ab1c21c53e8c346c9ef15806
maysqunaibi <maysqunaibi2000@gmail.com>
2020-03-18 15:42:10 +0200

    example 2

--- onlineStore.js
+++ onlineStore.js
@@ -1,8 +1,9 @@
 /**  
-*We design a system for an online store and we need your help to set the structure.
-*The store has different departments there’s:flowers department, clothes, makeup..etc.
-*Every department has a set of different products 
-*and every product has different properties such as: the price, name, department's name...and more.
+ * ************************************ Example 1 *******************************************
+ * We design a system for an online store and we need your help to set the structure.
+ * The store has different departments there’s:flowers department, clothes, makeup..etc.
+ * Every department has a set of different products 
+ * and every product has different properties such as: the price, name, department's name...and more.
 
                                     ###############################
 
@@ -14,4 +15,18 @@
 can you create a function that takes an array of department’s products and the percent of the discount as parameters.
 The function changes the price if the product price is more than 50$.
 
+ */
+/**
+ * ************************************ Example 2 *******************************************
+ * Students Management Program: Implement a program that manages a set of students. Each student has an ID, name, city, grade.
+ * You can store the student's information in array.
+ * The program should enable the following functionality
+
+#Edit student Grads:
+The function takes the id for the student. If the student with the given id is found, then the program sets a new grade for this student. 
+
+##The average grade of all students
+
+###All Students from the same city:
+ The function takes the city for the student and return an array of all students in this city.
  */
