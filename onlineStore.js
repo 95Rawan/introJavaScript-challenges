92f483f0704bd63a4f3e2b394d651c5b11d4e8de
maysqunaibi <maysqunaibi2000@gmail.com>
2020-03-18 13:51:39 +0200

    add descrption

---
+++ onlineStore.js
@@ -0,0 +1,17 @@
+/**  
+*We design a system for an online store and we need your help to set the structure.
+*The store has different departments there’s:flowers department, clothes, makeup..etc.
+*Every department has a set of different products 
+*and every product has different properties such as: the price, name, department's name...and more.
+
+                                    ###############################
+
+-Write 3 object products that have at least 3 properties.
+
+-Choose a department and create an array to store the department’s product on it.
+
+-Now the owner decided to make a discount on one of the department’s products
+can you create a function that takes an array of department’s products and the percent of the discount as parameters.
+The function changes the price if the product price is more than 50$.
+
+ */
